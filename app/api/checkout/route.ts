0e3cd07399909927b5c98d1fa3f7a29b11470039
N1kos9 <onkl3p@gmail.com>
2024-03-24 18:49:36 +0200

    cors politics #2

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin":
+    "https://ecommerce-frontend-coral-xi.vercel.app",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
